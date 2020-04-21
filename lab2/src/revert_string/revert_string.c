d901ff4a3c0934d8858440045a98ab1fb78e68fd
po1yb1ank <b00t.b00t@yandex.ru>
2020-04-21 20:24:44 +0300

    string_reverse

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,18 @@
 #include "revert_string.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	printf("called func\n");
+	int str_size = sizeof(char) * (strlen(str));
+	printf("%d str_size\n", str_size);
+	int str_ln = strlen(str) - 1;
+	char *reverse = malloc(str_size);//same length
+	for (int i = 0; i <= str_ln; i++){
+		reverse[i] = str[str_ln-i];
+	}
+	strcpy(str, reverse);
 }
 
